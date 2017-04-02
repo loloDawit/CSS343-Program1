472952929a45507896e8724c22fb71c827415f7f
Dawit Abera <dawitabera64@gmail.com>
2017-04-01 20:14:46 -0700

    CSS343- Program1-Doubly linked circular list

---
+++ polynomial/polynomial.h
@@ -0,0 +1,14 @@
+//
+//  polynomial.hpp
+//  polynomial
+//
+//  Created by Dawit Abera on 4/1/17.
+//  Copyright © 2017 University of Washington. All rights reserved.
+//
+
+#ifndef polynomial_hpp
+#define polynomial_hpp
+
+#include <stdio.h>
+
+#endif /* polynomial_hpp */
